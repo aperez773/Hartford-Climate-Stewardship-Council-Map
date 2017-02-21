38b3efc795ab5fcc74b2f66a9ceb1686fa845bec
aperez773 <alexis.perez@trincoll.edu>
2017-02-21 17:40:54 -0500

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1MFEmrxrBX_ePLtjgc0EcYiuZqy4iBaqI9Y2kTjzwaG0/edit#gid=0';
+var googleDocURL = '1MFEmrxrBX_ePLtjgc0EcYiuZqy4iBaqI9Y2kTjzwaG0';
